6c86e304baf58be83836b868b914eabb23efd80c
Ariel Evangelista <ariel.evangelista@outlook.com>
2025-05-04 18:45:33 -0300

    feat: ready or not here I come.

--- src/App.tsx
+++ src/App.tsx
@@ -9,16 +9,6 @@ import Ana from "./assets/ana.jpg";
 const gameState = {
   players: [
     {
-      id: "2",
-      name: "Ariel",
-      age: 27,
-      gender: "male",
-      photo: Ariel,
-      title: "Programador",
-      points: 0,
-      destination: "Los Angeles",
-    },
-    {
       id: "1",
       name: "Ana",
       age: 25,
@@ -27,6 +17,16 @@ const gameState = {
       title: "Terapeuta Ocupacional",
       points: 1000,
       destination: "Egito",
+    },
+    {
+      id: "2",
+      name: "Ariel",
+      age: 27,
+      gender: "male",
+      photo: Ariel,
+      title: "Programador",
+      points: 0,
+      destination: "Los Angeles",
     },
   ],
   currentDestination: "Egito",
@@ -55,7 +55,7 @@ function App() {
       {/* Header */}
       <header className="w-full py-6 px-4 text-center relative z-10">
         <div className="inline-flex items-center gap-2 bg-white/10 backdrop-blur-md px-6 py-3 rounded-full">
-          <MapPin className="text-blue-400" />
+          <MapPin className="text-pink-500" />
           <h1 className="font-montserrat text-2xl md:text-3xl text-white">
             Destino atual: Egito
           </h1>
